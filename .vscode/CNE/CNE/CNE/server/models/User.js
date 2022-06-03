db29afbf4abc5a46517e5882c5827d2d16b26410
MiyaChi98 <chintt.hrt@gmail.com>
2022-06-03 17:41:33 +0700

    ok

---
+++ .vscode/CNE/CNE/CNE/server/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  email: {
+    type: String,
+    required: 'This field is required.'
+  },
+  password: {
+    type: String,
+    required: 'This field is required.'
+  }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
